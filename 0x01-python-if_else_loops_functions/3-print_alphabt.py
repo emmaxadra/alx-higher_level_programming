85476e0569a3966829c0ced2468f212a558aaf85
emmaxadra <emmaxadra@gmail.com>
2022-11-29 05:33:21 -0800

    thd

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for character in range(97, 123):
-    if (character != 101 and character != 113):
-        print("{:c}".format(character), end='')
+for a in "abcdefghijklmnopqrstuvwxyz":
+    if(a not in "qe"):
+        print("{}".format(a), end='')
