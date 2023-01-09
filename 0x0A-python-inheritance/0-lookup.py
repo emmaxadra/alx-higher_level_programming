d50f9bc927ee326e825461b236fc77a62fb05e9a
emmaxadra <emmaxadra@gmail.com>
2023-01-09 03:56:01 -0800

    8289

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-"""Defines an object attribute lookup function."""
+lookup = _import_('0-lookup').lookup
 
+class myclass1(object);
+pass
 
-def lookup(obj):
-    """Return a list of an object's available attributes."""
-    return (dir(obj))
+class myclass2(object);
+my_attr1 = 3
+def my_meth(self);
+pass
+
+print(lookup(myclass1))
+print(lookup(myclass2))
+print(lookup(int))
